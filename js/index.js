dab7b3ea1c38595c1e1b7a90969ec8826a2eb19d
Gibson10 <njine10@gmail.com>
2022-12-05 10:10:37 -0500

    edited the counter
    edited the counter

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 1000; // The lower the slower
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Lower inc to slow and higher to slow
+    const inc = target / speed;
+
+    // Check if target is reached
+    if (count < target) {
+      // Add inc to count and output in counter
+      counter.innerText = Math.ceil(count + inc);
+      // Call function every ms
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
+
 //validate the contact us form
 const form = document.getElementById("form");
 const username = document.getElementById("username");
@@ -126,29 +151,4 @@ aboutimage.addEventListener("click", function () {
   console.log("clicked");
   aboutimage.style.transform = "rotate(180deg)";
   aboutimage.style.transition = "all 1s";
-});
-
-const counters = document.querySelectorAll(".counter");
-const speed = 200; // The lower the slower
-
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText;
-
-    // Lower inc to slow and higher to slow
-    const inc = target / speed;
-
-    // Check if target is reached
-    if (count < target) {
-      // Add inc to count and output in counter
-      counter.innerText = Math.ceil(count + inc);
-      // Call function every ms
-      setTimeout(updateCount, 1);
-    } else {
-      counter.innerText = target;
-    }
-  };
-
-  updateCount();
 });
